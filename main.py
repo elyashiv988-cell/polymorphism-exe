ed44cc9053690d7d997abad7a8e4d73c8aff8280
Elyashiv <elyashiv988@gmail.com>
2026-07-14 11:51:39 +0300

    added 1

---
+++ main.py
@@ -0,0 +1,21 @@
+# 1 
+class Device:
+    def __init__(self,name):
+        self.name=name
+    def activate(self):
+        print(f"Device {self.name} is now on. ")
+class SmartTV(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def activate(self):
+        print(f"TV {self.name} is playing the home screen")
+class SmartSeaker(Device):
+    def __init__(self, name):
+        super().__init__(name)
+    def activate(self):
+        print(f"Speaker {self.name} is ready to play music. ")
+tv=SmartTV("Samsung")
+tv.activate()
+
+speaker=SmartSeaker("Echo")
+speaker.activate()
