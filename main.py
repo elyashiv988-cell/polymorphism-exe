c0a120871101ede8dfbabbf952b5d78efdbaacec
Elyashiv <elyashiv988@gmail.com>
2026-07-14 12:21:14 +0300

    added 3

--- main.py
+++ main.py
@@ -41,3 +41,29 @@ bedroom=SmartLamp("Bedroom Lamp")
 bedroom.deactivate()        
 ac=SmartAC("Living Room AC")
 ac.deactivate()
+
+# 3 
+
+class Device:
+    def __init__(self,name,is_on):
+        self.name=name
+        self.is_on=is_on
+    def status(self):
+        print(f"{self.name}: on") if self.is_on else print(f"{self.name}: off")
+class SmartTV(Device):
+    def __init__(self, name, is_on,channel):
+        super().__init__(name, is_on)
+        self.channel=channel
+    def status(self):
+        print(f"{self.name}: on, watching chnnel {self.channel}") if self.is_on else print(f"{self.name}: off")
+class SmartSpeaker(Device):
+    def __init__(self, name, is_on,song):
+        super().__init__(name, is_on)
+        self.song=song
+    def status(self):
+        print(f"{self.name}: on, current song: {self.song}") if self.is_on else print(f"{self.name}: off")
+tv=SmartTV("LG",True,8)
+tv.status()
+speaker=SmartSpeaker("Alexa",True,"Bohemian Rhapsody")
+speaker.status()
+        
